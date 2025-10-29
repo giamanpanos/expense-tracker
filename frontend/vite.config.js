ddc8a5128b7abdfb228d59fdea5a6db3ae1a5119
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-10-30 00:00:39 +0200

    render fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    exclude: ["mini-svg-data-uri"],
+  },
 });
