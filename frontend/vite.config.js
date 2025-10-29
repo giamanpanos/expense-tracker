5de2445f4e00795fcfccbb9b0b75365fe7311f03
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-10-30 00:11:31 +0200

    packages fix 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    minify: false,
   },
 });
